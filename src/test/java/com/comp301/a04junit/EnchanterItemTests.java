5d1439a8f2f4ed245585a95a6ee2f88c30062853
anikash1232 <anirudh.vpk@gmail.com>
2025-10-13 00:40:56 -0400

    first try for encahter

--- src/test/java/com/comp301/a04junit/EnchanterItemTests.java
+++ src/test/java/com/comp301/a04junit/EnchanterItemTests.java
@@ -1,13 +1,43 @@
 package com.comp301.a04junit;
 
+import com.comp301.a04junit.adventure.ItemImpl;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /** Write unit tests for the ItemImpl class here */
 public class EnchanterItemTests {
+
   @Test
-  public void unitTest1() {
-    assertTrue(true); // TODO: Write your first unit test!
+  public void testGetName() {
+    ItemImpl i = new ItemImpl("nandan");
+    assertEquals("nandan", i.getName());
+  }
+
+  @Test
+  public void testEqualsSameName() {
+    ItemImpl i1 = new ItemImpl("aditya");
+    ItemImpl i2 = new ItemImpl("aditya");
+    assertTrue(i1.equals(i2));
+  }
+
+  @Test
+  public void testEqualsDifferentName() {
+    ItemImpl i1 = new ItemImpl("arya");
+    ItemImpl i2 = new ItemImpl("aditya");
+    assertFalse(i1.equals(i2));
+  }
+
+  @Test
+  public void testEqualsNullOrDifferentObject() {
+    ItemImpl i = new ItemImpl("arya");
+    assertFalse(i.equals(null));
+    assertFalse(i.equals("arya"));
+  }
+
+  @Test
+  public void testToString() {
+    ItemImpl i = new ItemImpl("nandan");
+    assertEquals("This is an item.", i.toString());
   }
 }
