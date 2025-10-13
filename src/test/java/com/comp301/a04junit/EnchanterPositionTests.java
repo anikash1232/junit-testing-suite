ec6676e73de42f80e6af18aae44f4ae19cf90fa4
anikash1232 <anirudh.vpk@gmail.com>
2025-10-13 00:57:53 -0400

    next try

--- src/test/java/com/comp301/a04junit/EnchanterPositionTests.java
+++ src/test/java/com/comp301/a04junit/EnchanterPositionTests.java
@@ -1,52 +1,42 @@
 package com.comp301.a04junit;
 
-import com.comp301.a04junit.adventure.Direction;
-import com.comp301.a04junit.adventure.Position;
 import com.comp301.a04junit.adventure.PositionImpl;
+import com.comp301.a04junit.adventure.Direction;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-/** Write unit tests for the PositionImpl class here */
 public class EnchanterPositionTests {
 
   @Test
-  public void testConstructorStoresValues() {
-    PositionImpl p = new PositionImpl(2, 5);
+  public void constructorStoresXY() {
+    PositionImpl p = new PositionImpl(2, 3);
     assertEquals(2, p.getX());
-    assertEquals(5, p.getY());
+    assertEquals(3, p.getY());
   }
 
   @Test
-  public void testGetNeighborNorth() {
-    PositionImpl p = new PositionImpl(3, 3);
-    Position north = p.getNeighbor(Direction.NORTH);
-    assertEquals(3, north.getX());
-    assertEquals(4, north.getY()); // y + 1
-  }
+  public void getNeighborMovesOneUnitCardinals() {
+    PositionImpl p = new PositionImpl(5, 10);
 
-  @Test
-  public void testGetNeighborSouth() {
-    PositionImpl p = new PositionImpl(4, 1);
-    Position south = p.getNeighbor(Direction.SOUTH);
-    assertEquals(4, south.getX());
-    assertEquals(0, south.getY()); // y - 1
-  }
+    PositionImpl east = (PositionImpl) p.getNeighbor(Direction.EAST);
+    assertEquals(6, east.getX());
+    assertEquals(10, east.getY());
 
-  @Test
-  public void testGetNeighborEast() {
-    PositionImpl p = new PositionImpl(0, 0);
-    Position east = p.getNeighbor(Direction.EAST);
-    assertEquals(1, east.getX());
-    assertEquals(0, east.getY());
-  }
-
-  @Test
-  public void testGetNeighborWest() {
-    PositionImpl p = new PositionImpl(5, 5);
-    Position west = p.getNeighbor(Direction.WEST);
+    PositionImpl west = (PositionImpl) p.getNeighbor(Direction.WEST);
     assertEquals(4, west.getX());
-    assertEquals(5, west.getY());
+    assertEquals(10, west.getY());
+
+    PositionImpl north = (PositionImpl) p.getNeighbor(Direction.NORTH);
+    assertEquals(5, north.getX());
+    assertEquals(11, north.getY());
+
+    PositionImpl south = (PositionImpl) p.getNeighbor(Direction.SOUTH);
+    assertEquals(5, south.getX());
+    assertEquals(9, south.getY());
+
+    // make sure original position doesn't change
+    assertEquals(5, p.getX());
+    assertEquals(10, p.getY());
   }
 }
