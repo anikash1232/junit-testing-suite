94996a1e380bb28987e1adeff4665a6228bb5fd4
anikash1232 <anirudh.vpk@gmail.com>
2025-10-13 00:17:51 -0400

    first push alphabetizer

--- src/test/java/com/comp301/a04junit/ApprenticeAlphabetizerTests.java
+++ src/test/java/com/comp301/a04junit/ApprenticeAlphabetizerTests.java
@@ -1,14 +1,52 @@
 package com.comp301.a04junit;
 
 
+import com.comp301.a04junit.alphabetizer.Alphabetizer;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /** Write tests for the Alphabetizer class here */
 public class ApprenticeAlphabetizerTests {
   @Test
-  public void unitTest1() {
-    assertTrue(true); // TODO: Write your first unit test!
+  public void testInOrder() {
+    String[] arr = {"charanjit", "anirudh", "bibek"};
+    Alphabetizer alpha = new Alphabetizer(arr);
+    assertEquals("anirudh", alpha.next());
+    assertEquals("bibek", alpha.next());
+    assertEquals("charanjit", alpha.next());
+  }
+
+  @Test
+  public void testHasNext() {
+    Alphabetizer alpha = new Alphabetizer(new String[]{"bibek", "anirudh"});
+    assertTrue(alpha.hasNext());
+    alpha.next();
+    assertTrue(alpha.hasNext());
+    alpha.next();
+    assertFalse(alpha.hasNext());
+  }
+
+  @Test
+  public void testNextWhenEmpty() {
+    Alphabetizer alpha = new Alphabetizer(new String[]{"bibek", "anirudh"});
+    alpha.next();
+    alpha.next();
+    try {
+      alpha.next();
+      fail("Shoud throw an exception if it is empty");
+    } catch (NoSuchElementException e) {
+    }
+  }
+
+  @Test
+  public void testDuplicates() {
+    String[] arr = {"anirudh", "bibek", "anirudh"};
+    Alphabetizer alpha = new Alphabetizer(arr);
+    assertEquals("anirudh", alpha.next());
+    assertEquals("anirudh", alpha.next());
+    assertEquals("bibek", alpha.next());
   }
 }
