46133a81a65a99aa1ef9b07504a63d9f73b1a393
anikash1232 <anirudh.vpk@gmail.com>
2025-10-13 00:48:01 -0400

    positon

--- src/test/java/com/comp301/a04junit/EnchanterPositionTests.java
+++ src/test/java/com/comp301/a04junit/EnchanterPositionTests.java
@@ -1,13 +1,52 @@
 package com.comp301.a04junit;
 
+import com.comp301.a04junit.adventure.Direction;
+import com.comp301.a04junit.adventure.Position;
+import com.comp301.a04junit.adventure.PositionImpl;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /** Write unit tests for the PositionImpl class here */
 public class EnchanterPositionTests {
   @Test
-  public void unitTest1() {
-    assertTrue(true); // TODO: Write your first unit test!
+  public void testConstructorStoresValues() {
+    PositionImpl p = new PositionImpl(2, 5);
+    assertEquals(2, p.getX());
+    assertEquals(5, p.getY());
+  }
+
+  @Test
+  public void testGetNeighborNorth() {
+    PositionImpl p = new PositionImpl(3, 3);
+    Position north = p.getNeighbor(Direction.NORTH);
+    assertEquals(3, north.getX());
+    assertEquals(2, north.getY());
+  }
+
+  @Test
+  public void testGetNeighborSouth() {
+    PositionImpl p = new PositionImpl(4, 1);
+    Position south = p.getNeighbor(Direction.SOUTH);
+    assertEquals(4, south.getX());
+    assertEquals(2, south.getY());
+  }
+
+  @Test
+  public void testGetNeighborEast() {
+    PositionImpl p = new PositionImpl(0, 0);
+    Position east = p.getNeighbor(Direction.EAST);
+    assertEquals(1, east.getX());
+    assertEquals(0, east.getY());
+  }
+
+  @Test
+  public void testGetNeighborWest() {
+    PositionImpl p = new PositionImpl(5, 5);
+    Position west = p.getNeighbor(Direction.WEST);
+    assertEquals(4, west.getX());
+    assertEquals(5, west.getY());
+
   }
 }
